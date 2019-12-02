f426395319409338ae102880a337ac755729f3b8
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2019-12-02 20:27:18 +0500

    Add files via upload

---
+++ task 1.c
@@ -0,0 +1,41 @@
+
+#include<stdio.h>
+main(){
+	int a,i=5,j=-1,f=4,count;
+	printf("enter number for rating \n");
+	scanf("%d",&a); 
+while(a<0)
+ {
+ printf("bye");
+	break;
+}
+	for(a;a<=f;a++)
+{
+	printf("thanks for your rating \n");
+		printf("enter number for rating \n");
+	scanf("%d",&a);
+}
+for(a;a>=i;a++)
+{
+printf("enter number for rating \n");
+	scanf("%d",&a);
+		count=count+1;
+	if (count==4)
+
+
+		printf("you have used all of youyr tries ");
+		printf("new user \n");
+printf("enter number for rating \n");
+	scanf("%d",&a); 
+	
+		
+		
+	
+	
+	
+	
+	
+}
+}
+
+
---
+++ task 2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main(){
+	int a,b,c,pp;
+	printf("enter your account number \n");
+	scanf("%d",&a);
+	while(a!=0)
+{
+		printf("enter your name \n");
+	scanf("%d",&b);
+		printf("enter your account number \n");
+	scanf("%d",&c);
+		printf("enter your purchase price \n");
+	scanf("%d",&pp);
+	
+
+printf("NAME = %d \n acount number = %d \n sale per month = %d",b,a,pp)	;
+	
+}
+while(a<=0)
+{
+	printf("log out");
+	break; 
+	  }
+}
+
+
+
