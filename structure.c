fc712ea593a97d9172e8979fdb9b2c62db0b7df1
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2019-12-24 15:03:14 +0500

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	
+	struct book {
+		char  name;
+		int roll_no;
+		int marks
+		
+		
+	}
+ b1,b2;	
+	
+	int i;
+	scanf("%s %d %d",&b1.name,&b1.roll_no,&b1.marks);
+printf("%s %d",&b1.name,b1.marks);
+	
+	
+	
+	
+	
+}
