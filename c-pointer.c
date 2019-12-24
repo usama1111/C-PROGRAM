83fafda80324c8f9820b6998b8e90540c896d55f
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2019-12-24 14:08:14 +0500

    Add files via upload

---
+++ c-pointer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int a=5;
+	 char b=4;
+	int *p=&a;   // variable ki data type likhengay
+	
+	printf("value of a is %d \n",a);
+	printf("address of  a is %u \n",&a);
+		printf("value of a is %d \n",*p);
+			printf("value of a is %u \n",p);	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
